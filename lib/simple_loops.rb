5cba3f0eb61d228f1af1579d461866eb221cd2f1
Will Lytle <wlytle89@gmail.com>
2020-08-12 23:52:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,15 @@
-# Write your methods here
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, n)
+  count = 0
+  while count < n do
+    puts message
+    count += 1
+  end
+end
+
+def output_array()
